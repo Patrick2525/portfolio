bba1b32fd7bf9a7dddb8126964b9eb4184ad2ec6
Patrick <knmswy2136@gmail.com>
2022-05-06 01:13:54 +0900

    05/05U

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,16 @@
 import { useNavigate } from "react-router-dom";
 //import { useEffect } from 'react';
 import "../styles/Menu.css";
+import { Link } from 'react-router-dom';
+//import { useHistory } from "react-router-dom";
 
 function Menu({title}) {
   const navigate = useNavigate();
+  //const history = useHistory();
 
   function handleClick() {
-    navigate(`/${title}`);
+    //navigate(`/${title}`);
+    
   }
   // useEffect(() => {
   //   handleClick();
@@ -14,7 +18,8 @@ function Menu({title}) {
 
   return (
     <div className='menu'>
-        <div className='menu-title' /*onClick={handleClick()}*/>
+        <div className='menu-title' onClick={() => navigate(`/${title}`)}>
+          <Link to={`/${title}`}/>
           {title}
         </div>
     </div>
